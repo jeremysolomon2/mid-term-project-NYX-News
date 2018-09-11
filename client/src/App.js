d0002f508905ac69abf4615a4441d4714d6dceb6
Jeremy Solomon <jeremysolomon2@gmail.com>
2018-09-11 10:54:13 -0400

    added header to all 3 pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link} from 'react-router-dom';
-// import axios from 'axios';
 import TopNews from './components/TopNews'
 import AllNews from './components/AllNews'
 import Header from './components/Header'
 import Home from './components/Home'
 import './css/App.css'
-
-// let picks = [];
 
 class App extends Component {
   constructor() {
@@ -25,43 +22,44 @@ class App extends Component {
   render() {
     return (
 
-<div><Home />
-      <BrowserRouter>
-        <div className='home-container'>
-        {/* <img src="/sitelogo.png"/> */}
-          <div className={this.state.displayForm}>
-            {/* <Header ☹️ /> */}
-            <h3>Pick 3!</h3>
-            <div>
-             <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Arts"); console.log(this.state.picks);}} }>Art</span>
-             <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Business"); console.log(this.state.picks);}} }>Business</span>
-            </div>
-            <div>
-              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Fashion"); console.log(this.state.picks);}} }>Fashion</span>
-              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Health"); console.log(this.state.picks);}} }>Health</span>
-            </div>
-            <div>
-              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Politics"); console.log(this.state.picks);}} }>Politics</span>
-              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Science"); console.log(this.state.picks);}} }>Science</span>
-            </div>
-            <div>
-              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Sports"); console.log(this.state.picks);}} }>Sports</span>
-              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Technology"); console.log(this.state.picks);}} }>Technology</span>
-            </div>
-            <div>
-              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Travel"); console.log(this.state.picks);}} }>Travel</span>
-              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("World"); console.log(this.state.picks);}} }>World</span>
-            </div>
-            <div className="sec sec-three">
-              <button className="btn btn-three"><Link onClick={this.toggleDisplay.bind(this)} to="/topnews">Next</Link></button>
-            </div>
-          </div>
-          <Route path="/topnews" component={() => <TopNews picks={this.state.picks} />}></Route> 
-          <Route path="/allnews" component={() => <AllNews />}></Route>     
-        </div>
-      </BrowserRouter></div>
-    );
-  }
+            <div><Home />
+                  <Header />
+                  <BrowserRouter>
+                    <div className='home-container'>
+                    {/* <img src="/sitelogo.png"/> */}
+                      <div className={this.state.displayForm}>
+                        {/* <Header ☹️ /> */}
+                        <h3>Pick 3!</h3>
+                        <div>
+                        <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Arts"); console.log(this.state.picks);}} }>Art</span>
+                        <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Business"); console.log(this.state.picks);}} }>Business</span>
+                        </div>
+                        <div>
+                          <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Fashion"); console.log(this.state.picks);}} }>Fashion</span>
+                          <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Health"); console.log(this.state.picks);}} }>Health</span>
+                        </div>
+                        <div>
+                          <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Politics"); console.log(this.state.picks);}} }>Politics</span>
+                          <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Science"); console.log(this.state.picks);}} }>Science</span>
+                        </div>
+                        <div>
+                          <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Sports"); console.log(this.state.picks);}} }>Sports</span>
+                          <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Technology"); console.log(this.state.picks);}} }>Technology</span>
+                        </div>
+                        <div>
+                          <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Travel"); console.log(this.state.picks);}} }>Travel</span>
+                          <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("World"); console.log(this.state.picks);}} }>World</span>
+                        </div>
+                        <div className="sec sec-three">
+                          <button className="btn btn-three"><Link onClick={this.toggleDisplay.bind(this)} to="/topnews">Next</Link></button>
+                        </div>
+                      </div>
+                      <Route path="/topnews" component={() => <TopNews picks={this.state.picks} />}></Route> 
+                      <Route path="/allnews" component={() => <AllNews />}></Route>     
+                    </div>
+                  </BrowserRouter></div>
+                );
+              }
 }
 
 export default App;
--- client/src/components/TopNews.js
+++ client/src/components/TopNews.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import SectionsTop from './SectionsTop';
-import Header from './Header';
-
 
 class TopNews extends Component {
 
   render() {
     return (
       <div className='wrapper'>  
-      <Header />
-      
         <div className='main'>
          {
             this.props.picks.map((pick, i) => {
