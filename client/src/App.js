78e1a7349dd5a9cbd0f0f14efc6aa3a250633569
Jeremy Solomon <jeremysolomon2@gmail.com>
2018-09-11 10:34:31 -0400

    moved picks to state

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,14 @@ import Header from './components/Header'
 import Home from './components/Home'
 import './css/App.css'
 
-let picks = [];
+// let picks = [];
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       displayForm: 'show',
+      picks: []
     }
   }
  
@@ -32,30 +33,30 @@ class App extends Component {
             {/* <Header ☹️ /> */}
             <h3>Pick 3!</h3>
             <div>
-             <span onClick={ () => {if (picks.length < 4) {picks.push("Arts"); console.log(picks);}} }>Art</span>
-             <span onClick={ () => {if (picks.length < 4) {picks.push("Business"); console.log(picks);}} }>Business</span>
+             <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Arts"); console.log(this.state.picks);}} }>Art</span>
+             <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Business"); console.log(this.state.picks);}} }>Business</span>
             </div>
             <div>
-              <span onClick={ () => {if (picks.length < 4) {picks.push("Fashion"); console.log(picks);}} }>Fashion</span>
-              <span onClick={ () => {if (picks.length < 4) {picks.push("Health"); console.log(picks);}} }>Health</span>
+              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Fashion"); console.log(this.state.picks);}} }>Fashion</span>
+              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Health"); console.log(this.state.picks);}} }>Health</span>
             </div>
             <div>
-              <span onClick={ () => {if (picks.length < 4) {picks.push("Politics"); console.log(picks);}} }>Politics</span>
-              <span onClick={ () => {if (picks.length < 4) {picks.push("Science"); console.log(picks);}} }>Science</span>
+              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Politics"); console.log(this.state.picks);}} }>Politics</span>
+              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Science"); console.log(this.state.picks);}} }>Science</span>
             </div>
             <div>
-              <span onClick={ () => {if (picks.length < 4) {picks.push("Sports"); console.log(picks);}} }>Sports</span>
-              <span onClick={ () => {if (picks.length < 4) {picks.push("Technology"); console.log(picks);}} }>Technology</span>
+              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Sports"); console.log(this.state.picks);}} }>Sports</span>
+              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Technology"); console.log(this.state.picks);}} }>Technology</span>
             </div>
             <div>
-              <span onClick={ () => {if (picks.length < 4) {picks.push("Travel"); console.log(picks);}} }>Travel</span>
-              <span onClick={ () => {if (picks.length < 4) {picks.push("World"); console.log(picks);}} }>World</span>
+              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("Travel"); console.log(this.state.picks);}} }>Travel</span>
+              <span onClick={ () => {if (this.state.picks.length < 3) {this.state.picks.push("World"); console.log(this.state.picks);}} }>World</span>
             </div>
             <div className="sec sec-three">
               <button className="btn btn-three"><Link onClick={this.toggleDisplay.bind(this)} to="/topnews">Next</Link></button>
             </div>
           </div>
-          <Route path="/topnews" component={() => <TopNews picks={picks} />}></Route> 
+          <Route path="/topnews" component={() => <TopNews picks={this.state.picks} />}></Route> 
           <Route path="/allnews" component={() => <AllNews />}></Route>     
         </div>
       </BrowserRouter></div>
