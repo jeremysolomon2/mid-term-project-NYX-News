fc541bbc81d9eea1f465fe4ea780ac3f40960acb
Jeremy Solomon <jeremysolomon2@gmail.com>
2018-09-11 22:10:33 -0400

    form done

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -12,20 +12,22 @@ class Form extends Component {
   }
   
   pushForm(ev) {
-    console.log(ev.target.dataset.category)
+    // console.log(ev.target.dataset.category)
     let category = ev.target.dataset.category
-    ev.target.classList.remove("btn-warning")
-    ev.target.classList.add("btn-primary")
+    // ev.target.classList.remove("btn-warning")
+    // ev.target.classList.add("btn-primary")
     this.props.pushPicks(category)
   }
 
   componentDidMount(prevProps, newProps){
     console.log(this.props.picks)
     this.props.picks.map((pick)=>{
-      if(pick == "Arts"){
-        this.myDiv.classList.remove("btn-warning");
-        this.myDiv.classList.add("btn-primary");
-      }
+      this[pick].classList.remove("btn-warning");
+      this[pick].classList.add("btn-primary");
+      // if(pick == "Arts"){
+      //   this.art.classList.remove("btn-warning");
+      //   this.art.classList.add("btn-primary");
+      // }
     })
   }
 
@@ -40,24 +42,24 @@ class Form extends Component {
                   <img src="/sitelogo2.png" alt="na"></img>
                 </div>
               <h3>Pick 3!</h3>
-                <span ref={(ele) => this.myDiv = ele} className="btn btn-warning" onClick={ this.pushForm } data-category="Arts">Art</span>
-                <span className="btn btn-warning" onClick={ this.pushForm } data-category='Business'>Business</span> 
+                <span ref={(ele) => this.arts = ele} className="btn btn-warning" onClick={ this.pushForm } data-category="arts">Art</span>
+                <span ref={(ele) => this.business = ele}  className="btn btn-warning" onClick={ this.pushForm } data-category='business'>Business</span> 
               </div>
               <div>
-                <span className="btn btn-warning" onClick={ () => {this.props.pushPicks('Fashion')} }>Fashion</span>
-                <span className="btn btn-warning" onClick={ () => {this.props.pushPicks('Health')} }>Health</span> 
+                <span ref={(ele) => this.fashion = ele}  className="btn btn-warning" onClick={ this.pushForm } data-category='fashion'>Fashion</span>
+                <span ref={(ele) => this.health = ele}  className="btn btn-warning" onClick={ this.pushForm } data-category='health'>Health</span> 
               </div>
               <div>
-                <span className="btn btn-warning" onClick={ () => {this.props.pushPicks('Politics')} }>Politics</span>
-                <span className="btn btn-warning" onClick={ () => {this.props.pushPicks('Science')} }>Science</span> 
+                <span ref={(ele) => this.politics = ele}  className="btn btn-warning" onClick={ this.pushForm } data-category='politics'>Politics</span>
+                <span ref={(ele) => this.science = ele}  className="btn btn-warning" onClick={ this.pushForm } data-category='science'>Science</span> 
               </div>
               <div>
-                <span className="btn btn-warning" onClick={ () => {this.props.pushPicks('Sports')} }>Sports</span>
-                <span className="btn btn-warning" onClick={ () => {this.props.pushPicks('Technology')} }>Technology</span> 
+                <span ref={(ele) => this.sports = ele} className="btn btn-warning" onClick={ this.pushForm } data-category='sports'>Sports</span>
+                <span ref={(ele) => this.technology = ele} className="btn btn-warning" onClick={ this.pushForm } data-category='technology'>Technology</span> 
               </div>
               <div>
-                <span className="btn btn-warning" onClick={ () => {this.props.pushPicks('Travel')} }>Travel</span>
-                <span className="btn btn-warning" onClick={ () => {this.props.pushPicks('World')} }>World</span> 
+                <span ref={(ele) => this.travel = ele} className="btn btn-warning" onClick={ this.pushForm } data-category='travel'>Travel</span>
+                <span ref={(ele) => this.world = ele} className="btn btn-warning" onClick={ this.pushForm } data-category='world'>World</span> 
               </div>
               <div className="sec sec-three">
                 <button className="btn btn-three"><Link to="/topnews">Next</Link></button>
